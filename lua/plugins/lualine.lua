296ef65884c9f3230be3a536e1741f0fb187efd8
NehemiahAklil <nehemiahaklil@gmail.com>
2024-10-08 04:22:04 +0300

    Some fine tweaking for pereference

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,21 +1,24 @@
 local M = {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
 }
 
 M.config = function()
-	require("lualine").setup({
-		sections = {
-			lualine_c = {
-				{
-					"filename",
-					path = 1,
-				},
-			},
-		},
-	})
+    require("lualine").setup({
+        options = {
+            theme = 'ayu',
+        },
+        sections = {
+            lualine_c = {
+                {
+                    "filename",
+                    path = 1,
+                },
+            },
+        },
+    })
 end
 
 return M
